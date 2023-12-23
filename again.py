693f2e51547362710c6d269da6ad5c4dd17f1c08
V Tsar <vastsareg@gmail.com>
2023-12-23 13:26:53 +0300

    почти готово

--- again.py
+++ again.py
@@ -2,7 +2,6 @@ import sys
 import sqlite3
 
 from PyQt5 import uic
-from PyQt5 import QtCore, QtWidgets
 from PyQt5.QtWidgets import QApplication, QDialog, QMainWindow
 from datetime import datetime
 
@@ -27,15 +26,16 @@ class Game(QMainWindow):
     def get_result(self):
         '''"Вытаскивает" нужный текст из таблицы'''
         cur = self.con.cursor()
-        self.text_screen = self.additional_text + '\n' + cur.execute("""SELECT text FROM Screens
+        self.text_screen = self.additional_text + cur.execute("""SELECT text FROM Screens
         WHERE id = ?""", (self.current_id,)).fetchone()[0]
         self.slide_text.setText(self.text_screen)
         self.text_slide = cur.execute("""SELECT text FROM Choices
         WHERE screen_id = ?""", (self.current_id,)).fetchone()[0]
-        self.text_button = cur.execute("""SELECT text, next_screen FROM Buttons
+        self.text_button = cur.execute("""SELECT text, next_screen, additional_text FROM Buttons
         WHERE choice_id = ?""", (self.current_id,)).fetchall()
-        self.text_button, self.next_slides_id = (list(map(lambda x: x[0], self.text_button)),
-                                                 list(map(lambda x: x[1], self.text_button)))
+        self.text_button, self.next_slides_id, self.additional_texts = (list(map(lambda x: x[0], self.text_button)),
+                                                                        list(map(lambda x: x[1], self.text_button)),
+                                                                        list(map(lambda x: x[2], self.text_button)))
         self.pushWelcome.setText('Продолжить')
         self.pushWelcome.clicked.connect(self.write)
 
@@ -45,7 +45,8 @@ class Game(QMainWindow):
             self.slide = Slide(text_slide=self.text_slide,
                                text_button=self.text_button,
                                next_slides_id=self.next_slides_id,
-                               game=self)
+                               game=self,
+                               additional_texts=self.additional_texts)
             self.slide.show()
             self.clickable = False
 
@@ -56,19 +57,20 @@ class Game(QMainWindow):
 
     def insert_statistics(self):
         '''Добавляет номер игрока, время и концовку в таблицу'''
-        #self.time_ = datetime.now().time - self.start_time
+        # self.time_ = datetime.now().time - self.start_time
         statistics_insert_statistics = '''INSERT INTO Statistics (time,end) VALUES(self.time_, self.end_)'''
         # time_ - время / end_ - номер концовки
 
 
 class Slide(QDialog):
-    def __init__(self, text_slide, text_button, next_slides_id, game):
+    def __init__(self, text_slide, text_button, next_slides_id, game, additional_texts):
         super().__init__()
         uic.loadUi('dialog.ui', self)
         self.text_slide = text_slide
         self.text_button = text_button
         self.next_slides_id = next_slides_id
         self.game = game
+        self.additional_texts = additional_texts
         self.label.setText(self.text_slide)
         self.push_button_1.setText(self.text_button[0])
         self.push_button_2.setText(self.text_button[1])
@@ -84,6 +86,11 @@ class Slide(QDialog):
             self.hide()
             self.game.current_id = self.next_slides_id[num]
             self.game.clickable = True
+            if self.additional_texts[num]:
+                self.game.additional_text = self.additional_texts[num] + '\n'
+            else:
+                self.game.additional_text = ''
+
         return press_info
 
     def draw_screen(self):
